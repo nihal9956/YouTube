f8c2561958e6d3fd19361b65e576edfe410da806
unknown <nihaltiwari28@gmail.com>
2020-10-06 07:53:03 -0700

    Updates

---
+++ precache-manifest.892c721b2d2ec56b97451b726a04ba9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbc44842a4ba5344726b9c69bfb706b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b1050ef8879b29bcb9e",
+    "url": "/static/css/main.bc35e9a5.chunk.css"
+  },
+  {
+    "revision": "4979e47635d0b75be088",
+    "url": "/static/js/2.65506f8a.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.65506f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1050ef8879b29bcb9e",
+    "url": "/static/js/main.c8f0d2a4.chunk.js"
+  },
+  {
+    "revision": "d4d17b874f55f22c4df9",
+    "url": "/static/js/runtime-main.18ffd322.js"
+  }
+]);
